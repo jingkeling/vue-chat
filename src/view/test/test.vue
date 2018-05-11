a76a27dd1f6edb8f90bc61d935425ff29c7b0cc9
jingkeling <841127052@qq.com>
2018-05-12 00:19:00 +0800

    h5 video测试

--- src/view/test/test.vue
+++ src/view/test/test.vue
@@ -1,23 +1,56 @@
 <template>
-    <div>
-      <i-button type="primary" @click="open(false)">打开提醒</i-button>
-      <i-button @click="open(true)">打开提醒（仅标题）</i-button>
+    <div class="test-container">
+      <video
+        id="theVideo"
+        class="video-player"
+        @click="playOrPause()"
+        preload="auto" src="http://192.168.1.109:8082/girl.mp4"
+        webkit-playsinline="true"
+        playsinline="true"
+        x5-video-player-type="h5"
+        x5-video-player-fullscreen="true"
+        x5-video-orientation="portraint">
+      </video>
+
     </div>
 </template>
 
 <script>
     export default {
-      methods: {
-        open (nodesc) {
-          this.$Notice.open({
-            title: '这是通知标题',
-            desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
-          });
+      data(){
+        return {
+          video:''
         }
+      },
+      mounted(){
+        this.video = document.getElementById("theVideo");
+      },
+      methods: {
+
+        geVideo(){
+          let url = "http://cn-sxxa-dx-v-03.acgvideo.com/vg7/9/7d/6689771-1-hd.mp4?expires=1489820100&platform=pc&ssig=QUB9WZ97u8YujcGAlcTxNg&oi=1855518653&nfa=iCnd1wqqoAsXLyv+e2tfVA==&dynamic=1";
+        },
+        playOrPause() {
+          this.video.paused ? this.video.play() : this.video.pause();
+        }
+
       }
     }
 </script>
 
 <style scoped>
+
+  .test-container {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+  }
+  .video-player{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    /*height: 100%;*/
+    object-fit: contain;
+  }
 
 </style>
